74e2c3413b1b5e400c52a6be922d6c100eaba79d
siyoon <ryuu.public@gmail.com>
2024-07-29 21:57:53 +0900

    test: main method runs Spring application

--- src/test/java/com/example/pokemon/PokemonApplicationTests.java
+++ src/test/java/com/example/pokemon/PokemonApplicationTests.java
@@ -1,10 +1,33 @@
 package com.example.pokemon;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class PokemonApplicationTests {
+
+    @Test
+    void testMainMethodRunsSpringApplication() {
+        try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
+            mockedSpringApplication.when(() -> SpringApplication.run(PokemonApplication.class, new String[] {}))
+                    .thenAnswer(invocation -> {
+                        invocation.getArguments();
+                        ConfigurableApplicationContext context = mock(ConfigurableApplicationContext.class);
+                        assertNotNull(context);
+                        return context;
+                    });
+
+            PokemonApplication.main(new String[] {});
+        }
+    }
 
     @Test
     void contextLoads() {
