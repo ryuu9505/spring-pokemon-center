e163544ba5f9309b0c5792687b45ecf7584aebb2
siyoon <ryuu.public@gmail.com>
2024-07-30 21:53:28 +0900

    chore: optimize imports

--- src/test/java/com/example/pokemon/PokemonApplicationTests.java
+++ src/test/java/com/example/pokemon/PokemonApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.pokemon;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
