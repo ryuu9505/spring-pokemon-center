fa13dc5d2c9bb9518bb9baf0f94932e500f790ab
siyoon <ryuu.public@gmail.com>
2024-07-30 21:52:26 +0900

    chore: optimize imports

--- src/main/java/com/example/pokemon/pokemon/Pokemon.java
+++ src/main/java/com/example/pokemon/pokemon/Pokemon.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
--- src/main/java/com/example/pokemon/trainer/PokemonTrainer.java
+++ src/main/java/com/example/pokemon/trainer/PokemonTrainer.java
@@ -1,17 +1,11 @@
 package com.example.pokemon.trainer;
 
-import com.example.pokemon.pokemon.PokemonType;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Builder
